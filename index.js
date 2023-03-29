a33ffe5131ca4de49a3f1ef3fc7c7e7f6e07f6cb
sadeeq fahass <sadeeqfhassan@gmail.com>
2023-03-29 21:17:33 +0100

    Add favicon, copyright and some design changes

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 function generate() {
-    var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
-    var newColor1 = "#";
-    var newColor2 = "#";
-    var degArr = ["20", "40", "45", "60", "55", "90", "180", "240"]
+    let hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    let newColor1 = "#";
+    let newColor2 = "#";
+    let deg = Math.ceil(Math.random() * 36) * 10
     
-    rndmNum = Math.floor((Math.random() * degArr.length))
-    num = degArr[rndmNum]
-    
+    // document.getElementById("colors-text").style.display = "block"
     
     for (let i = 0; i < 6; i++) {
-        var x = Math.round(Math.random() * 15)
-        var a = Math.round(Math.random() * 15)
-        var y = hexValues[x]
-        var b = hexValues[a]
+        let x = Math.round(Math.random() * 15)
+        let a = Math.round(Math.random() * 15)
+        let y = hexValues[x]
+        let b = hexValues[a]
         newColor1 += y
         newColor2 += b
     }
-    document.getElementById("container").style.backgroundImage = `linear-gradient(${num}deg, ${newColor1}, ${newColor2})`
-    document.getElementById("output").innerHTML = newColor1 
-    document.getElementById("output2").innerHTML = newColor2
+    document.getElementById("container").style.backgroundImage = `linear-gradient(${deg}deg, ${newColor1}, ${newColor2})`
+    // document.getElementById("output").innerHTML = newColor1 
+    // document.getElementById("output2").innerHTML = newColor2
     document.getElementById("result").innerHTML = `
-    linear-gradient(${num}deg, ${newColor1}, ${newColor2});`
-
-
-       
+    linear-gradient(${deg}deg, ${newColor1}, ${newColor2});`
     
 }
